0a77dcb8d5c87f92bdde4cbd4abd4f3521cd9055
jbtrask <jbt_5@yahoo.com>
2013-03-29 21:44:45 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jbtiki::Application.config.secret_token = '5c569768abea0366696b943432143d70f264242424b7fc5f0a744bd2c19a0e2687600eeba42276d21224c8e22f9fced210f87b73e7c9b68b64279b2add18251d'
